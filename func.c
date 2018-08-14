ff4263435b0eb0989a91581b71efdf087480f6cf
Edward Manda <lilkups@yahoo.com>
2018-08-13 19:04:12 -0500

    More stuff added

---
+++ func.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void hello(char *firstname, char *lastname)
+{
+  printf("Hello %s %s \n", firstname, lastname);
+}
+
+int main(int argc, char **argv)
+{
+  // init a name variable
+  // char *name;
+  char firstname[10];
+  char lastname[10];
+  // prompt user for input
+  printf("What is your name? ");
+  // scanf accepts input
+  scanf("%s %s", firstname, lastname);
+  // print the results
+  printf("You entered %s %s \n", firstname, lastname);
+
+  hello(firstname, lastname);
+  return 0;
+}
